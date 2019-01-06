d16461533a37b9828df3f69db18de29968421b70
jormd <johann.romand@etudiant.univ-reims.fr>
2019-01-06 14:36:53 +0100

    création de la matrice et possitionnement des lettre par rapport à la fréquence

--- js/cryptanalyse.js
+++ js/cryptanalyse.js
@@ -1,38 +1,106 @@
+const frLetterApparition = ['e', 'a', 'i', 's', 'n', 'r'];
+
+/**
+ * Permet de transformer le text en tableau contenant que 2 caractères par cellule
+ * @param text
+ * @returns {*}
+ */
 function decoupage(text){
     return text.match(/.{1,2}/g);
 }
 
+/**
+ * permet de trouver les doublon pour utilisé la fréquence dessus
+ * @param text
+ * @returns {Array}
+ */
 function searchDoublon(text){
     let tab = decoupage(text);
-    console.log(tab);
     let doublon = [];
+    let compt = 0;
 
     for(let i = 0; i<tab.length; i++){
         if(doublon[tab[i]] == null){
             for(let a = i+1; a<tab.length; a++){
                 if(tab[i] === tab[a]){
                     if(doublon[tab[i]] != null){
-                        doublon[tab[i]] = doublon[tab[i]]+1;
+                        doublon[compt][1] = doublon[compt][1]+1;
                     }
                     else{
-                        doublon[tab[i]] = 2;
+                        doublon[compt] = [];
+                        doublon[compt][0] = tab[i];
+                        doublon[compt][1] = 2;
+                        compt++;
                     }
                 }
             }
         }
     }
-    console.log(doublon);
-    console.log(possibility(doublon));
+
+    return rangementParnbApparition(doublon);
+}
+
+/**
+ * Méthode permettant de rangé le tableau des doublon par rapport à la fréquence d'apparistion (mode décroissant, plus grand au plus petit)
+ * @param doublon
+ * @param nb
+ * @returns {*}
+ */
+function rangementParnbApparition(doublon, nb = 1) {
+    for (var i = 0; i < doublon.length; i++){
+        if(i+1 < doublon.length){
+            if(doublon[i+1][1] > doublon[i][1]){
+                var tmp = doublon[i];
+                doublon[i] = doublon[i+1];
+                doublon[i+1] = tmp;
+            }
+        }
+    }
+
+    if(nb < doublon.length-1){
+        rangementParnbApparition(doublon, nb+1);
+    }
+
     return doublon;
 }
 
-function possibility(array){
-    console.log(Math.max(...array));
-    console.log(array)
+/**
+ * Méthode permettant de positionner les lettres par rapport à la fréquence d'apparition
+ * @param doublon
+ * @returns {Array}
+ */
+function positionnementLettre(doublon) {
+    let tab = [];
+    for (var i = 0; i < doublon.length; i++){
+        for (var b=0; b<6; b++){
+            // pour ne pas multiplie le nombre de champs en ligne
+            if(tab[b] == null) {
+                tab[b] = [];
+            }
+            for (var a=0; a<6; a++){
+                if(doublon[i][0] === alphabetCaractSpecial[b]+alphabetCaractSpecial[a]){
+                    tab[b][a] = frLetterApparition[i];
+                }
+                // pour ne pas multiplie le nombre de champs en ligne
+                else if(tab[b].length < 6){
+                    tab[b].push('');
+                }
+            }
+        }
+    }
+    return tab
 }
 
 function createMatriceAletoire(text) {
-    let doublon = searchDoublon(text);
-
+    let tab = positionnementLettre(searchDoublon(text));
+    // var compt = 0;
+    //
+    // for (var i=0; i<6; i++){
+    //     tab[i] = [];
+    //     for (var a=0; a<6; a++){
+    //         tab[i].push();
+    //         compt ++;
+    //     }
+    // }
 
 }
